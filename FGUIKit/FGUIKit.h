34792ce3a48e2f387a1f691e35f23f2693c141db
liguojun <liguojun@bogon>
2019-09-26 17:24:06 +0800

    first commit

---
+++ FGUIKit/FGUIKit.h
@@ -0,0 +1,19 @@
+//
+//  FGUIKit.h
+//  FGUIKit
+//
+//  Created by 肥儿古德 on 2019/9/26.
+//  Copyright © 2019 hh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FGUIKit.
+FOUNDATION_EXPORT double FGUIKitVersionNumber;
+
+//! Project version string for FGUIKit.
+FOUNDATION_EXPORT const unsigned char FGUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FGUIKit/PublicHeader.h>
+
+
