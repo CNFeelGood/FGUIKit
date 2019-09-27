caae64764b6ef1a697474284a6f8461351852ec0
liguojun <liguojun@bogon>
2019-09-27 17:27:58 +0800

    添加tabbarCEasySetup

--- FGUIKit/FGUIKit.h
+++ FGUIKit/FGUIKit.h
@@ -16,5 +16,6 @@ FOUNDATION_EXPORT const unsigned char FGUIKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <FGUIKit/PublicHeader.h>
 #import <FGUIKit/FGViewController.h>
+#import <FGUIKit/UITabBarController+FGEasySetup.h>
 
 
