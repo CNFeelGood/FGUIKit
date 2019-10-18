98ebc54a3bc97c83d6c752664a2fe2f2d75651ca
liguojun <liguojun@bogon>
2019-10-18 18:15:56 +0800

    添加导航栏视图的rect分类

---
+++ FGUIKit/UIView/UIView+FGNavBarSubViewRect.h
@@ -0,0 +1,17 @@
+//
+//  UIView+FGNavBarSubViewRect.h
+//  FGUIKit
+//
+//  Created by 肥儿古德 on 2019/10/18.
+//  Copyright © 2019 hh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (FGNavBarSubViewRect)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FGUIKit/UIView/UIView+FGNavBarSubViewRect.m
@@ -0,0 +1,17 @@
+//
+//  UIView+FGNavBarSubViewRect.m
+//  FGUIKit
+//
+//  Created by 肥儿古德 on 2019/10/18.
+//  Copyright © 2019 hh. All rights reserved.
+//
+
+#import "UIView+FGNavBarSubViewRect.h"
+
+@implementation UIView (FGNavBarSubViewRect)
+
+- (CGSize)intrinsicContentSize{
+    return UILayoutFittingExpandedSize;
+}
+
+@end
