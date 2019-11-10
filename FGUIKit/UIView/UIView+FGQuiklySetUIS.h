8bed0edb71f9e378e50e7c2548b862d007d48479
liguojun <liguojun@bogon>
2019-11-10 11:06:16 +0700

    uiview分类

---
+++ FGUIKit/UIView/UIView+FGQuiklySetUIS.h
@@ -0,0 +1,20 @@
+//
+//  UIView+FGQuiklySetUIS.h
+//  FGUIKit
+//
+//  Created by 肥儿古德 on 2019/11/10.
+//  Copyright © 2019 hh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+//#import <MASConstraintMaker.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (FGQuiklySetUIS)
+
+//+ (instancetype)viewAddToSuperView:(UIView *)view fg_makeConstraints:(void(^)(MASConstraintMaker *make))block;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FGUIKit/UIView/UIView+FGQuiklySetUIS.m
@@ -0,0 +1,20 @@
+//
+//  UIView+FGQuiklySetUIS.m
+//  FGUIKit
+//
+//  Created by 肥儿古德 on 2019/11/10.
+//  Copyright © 2019 hh. All rights reserved.
+//
+
+#import "UIView+FGQuiklySetUIS.h"
+
+@implementation UIView (FGQuiklySetUIS)
+
+//+ (instancetype)viewAddToSuperView:(UIView *)view fg_makeConstraints:(void (^)(MASConstraintMaker *))block{
+//    UIView * aView = [[self class] new];
+//    [view addSubview:aView];
+//    [aView mas_makeConstraints:block];
+//    return aView;
+//}
+
+@end
