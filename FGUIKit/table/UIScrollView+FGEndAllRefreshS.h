5a6b1bd439107a3a365939982ce682eb822049ba
liguojun <liguojun@bogon>
2019-11-11 23:02:02 +0700

    完善

---
+++ FGUIKit/table/UIScrollView+FGEndAllRefreshS.h
@@ -0,0 +1,26 @@
+//
+//  UIScrollView+FGEndAllRefresh.h
+//  FGUIKit
+//
+//  Created by 肥儿古德 on 2019/11/11.
+//  Copyright © 2019 hh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIScrollView (FGEndAllRefreshS)
+
+//- (void)endAllRefreshing;
+//- (void)endAllRefreshingByHasMoreData:(BOOL)has;
+
+@end
+
+//@interface MJRefreshFooter (FeelGoodEndRefresh)
+//
+//- (void)endRefreshingByHasMoreData:(BOOL)has;
+//
+//@end
+
+NS_ASSUME_NONNULL_END
---
+++ FGUIKit/table/UIScrollView+FGEndAllRefreshS.m
@@ -0,0 +1,34 @@
+//
+//  UIScrollView+FGEndAllRefresh.m
+//  FGUIKit
+//
+//  Created by 肥儿古德 on 2019/11/11.
+//  Copyright © 2019 hh. All rights reserved.
+//
+
+#import "UIScrollView+FGEndAllRefresh.h"
+
+@implementation UIScrollView (FGEndAllRefreshS)
+
+//- (void)endAllRefreshing{
+//    [self.mj_header endRefreshing];
+//    [self.mj_footer endRefreshing];
+//}
+//- (void)endAllRefreshingByHasMoreData:(BOOL)has{
+//    [self.mj_header endRefreshing];
+//    [self.mj_footer endRefreshingByHasMoreData:has];
+//}
+
+@end
+
+//@implementation MJRefreshFooter (FeelGoodEndRefresh)
+//
+//- (void)endRefreshingByHasMoreData:(BOOL)has{
+//    if (has) {
+//        [self endRefreshing];
+//    }else{
+//        [self endRefreshingWithNoMoreData];
+//    }
+//}
+//
+//@end
